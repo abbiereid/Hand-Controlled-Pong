b03b7be36ae38d6e48e8be671b39dd121fdf9fce
Abbie Reid <72096268+abbiereid@users.noreply.github.com>
2025-02-15 22:36:49 +0000

    .

--- Pong/src/main/java/com/almasb/fxglgames/pong/HandTrackingUIController.java
+++ Pong/src/main/java/com/almasb/fxglgames/pong/HandTrackingUIController.java
@@ -6,18 +6,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.paint.Color;
 
 public class HandTrackingUIController implements UIController {
-
-    @FXML
-    private Canvas canvas;
-
-    public Canvas getCanvas() {
-        return canvas;
-    }
-
     @Override
     public void init() {
-       canvas.getGraphicsContext2D().setFill(Color.WHITE);
-       canvas.setHeight(600);
-       canvas.setWidth(800);
+       Canvas canvas = new Canvas(800, 600);
     }
 }
